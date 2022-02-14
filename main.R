fb52ccd473bf581ee4713ae0747ea64bdad4004a
Huy Sam <huyprolove114@gmail.com>
2022-02-14 04:42:56 -0500

    Add files via upload

---
+++ main.R
@@ -0,0 +1,32 @@
+install.packages("forecast")
+library("forecast")
+
+df <- read.csv("C:/Users/huy sam/Desktop/Airfares.csv")
+dim(df)
+names(df)
+
+selected.var <- c("S_CODE","S_CITY","E_CODE","E_CITY","COUPON","NEW","VACATION_YES",
+                  "VACATION_NO","SW_YES","SW_NO","HI","S_INCOME",
+                  "E_INCOME","S_POP","E_POP","SLOT_FREE","SLOT_CTRL","GATE_FREE",
+                  "GATE_CONS","DISTANCE","PAX","FARE")
+df$FARE = as.numeric(gsub("\\$","",df$FARE)) #REMOVE $ SIGN
+#question a
+set.seed(1)  
+train.index <- sample(c(1:638), 383)  
+train.df <- df[train.index, selected.var]
+valid.df <- df[-train.index, selected.var]
+dim(train.df)
+dim(valid.df)
+#question b
+
+model1<- lm(FARE ~ COUPON + NEW + VACATION_YES + SW_YES + HI + S_INCOME + E_INCOME + S_POP + E_POP 
+            + SLOT_CTRL + GATE_CONS + DISTANCE + PAX,data = train.df) 
+summary(model1)
+model1.pred <- predict(model1, valid.df)
+accuracy(model1.pred, valid.df$FARE)
+
+model1.step <- step(model1, direction = "backward")
+sum_back<-summary(model1.step)
+
+model1.step.pred <- predict(model1.step, valid.df)
+accuracy(model1.step.pred, valid.df$FARE)
